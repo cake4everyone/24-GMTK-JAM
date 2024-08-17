84cef031837aa3615c0322275b566ba8715d87e9
Kesuaheli <kesuaheligame@gmail.com>
2024-08-17 14:59:16 +0200

    chaged resizing stuff
    added cursor icons
    
    Co-Authored-By: Niraculix <75939533+Niraculix@users.noreply.github.com>

--- WindowManager.gd
+++ WindowManager.gd
@@ -2,15 +2,15 @@ extends Control
 
 var colShape: CollisionShape2D
 
-var start: Vector2
-var initialPos: Vector2
-var isMoving := false
-var isResizing := false
-var resizeX: bool
-var resizeY: bool
-var initialSize: Vector2
-var newWidth := 1
-var newHeight := 1
+var camPreviousPos: Vector2
+var resizeLeft: bool
+var resizeRight: bool
+var resizeTop: bool
+var resizeBottom: bool
+
+var deactivated := false
+
+const BORDER_SIZE := 10
 
 func _ready():
 	colShape = get_node("StaticBody2D/CollisionShape2D")
@@ -18,70 +18,141 @@ func _ready():
 	colShape.shape.size = Vector2(get_size().x, get_size().y)
 	colShape.global_position = Vector2(get_position().x + (get_size().x / 2), get_position().y + (get_size().y / 2))
 	print("2: " + str(colShape))
+	$Mouse.position = Vector2(-BORDER_SIZE, -BORDER_SIZE)
+	$Mouse.size = get_size() + Vector2(2 * BORDER_SIZE, 2 * BORDER_SIZE)
 
-func _input(event):
-	if Input.is_action_just_pressed("LeftMouseDown"):
-		var rect: Rect2 = get_global_rect()
-		var localMousePos: Vector2 = event.position - get_global_position()
-		
-		if (localMousePos.x < rect.size.x && localMousePos.x >= rect.size.x - 20 && localMousePos.y > 0 && localMousePos.y < rect.size.y):
-			start.x = event.position.x
-			initialSize.x = get_size().x
-			resizeX = true
-			isResizing = true
-			print("1 right")
+func _process(_delta):
+	print("%s left: %s, right: %s, top: %s, bottom: %s" % [str(colShape), is_on_left_border(), is_on_right_border(), is_on_top_border(), is_on_bottom_border()])
+
+	if resizeRight || resizeLeft || resizeBottom || resizeTop:
+		var newSize: Vector2 = get_size()
+		var newPos: Vector2 = get_position()
+
+		var camRelative: Vector2 = camPreviousPos - %Player/Camera2D.get_screen_center_position()
+
+		if resizeRight:
+			newSize.x -= camRelative.x
+		if resizeBottom:
+			newSize.y -= camRelative.y
 			
-		if (localMousePos.y < rect.size.y && localMousePos.y >= rect.size.y - 20 && localMousePos.x > 0 && localMousePos.x < rect.size.x):
-			start.y = event.position.y
-			initialSize.y = get_size().y
-			resizeY = true
-			isResizing = true
-			print("2 down")
+		if resizeLeft:
+			newSize.x += camRelative.x
+			newPos.x -= camRelative.x
+			set_position(newPos)
 			
-		if (localMousePos.x > 0 && localMousePos.x <= 20 && localMousePos.y > 0 && localMousePos.y < rect.size.y):
-			start.x = event.position.x
-			initialPos.x = get_global_position().x
-			initialSize.x = get_size().x
-			resizeX = true
-			isResizing = true
-			print("3 left")
-			
-		if (localMousePos.y > 0 && localMousePos.y <= 20 && localMousePos.x > 0 && localMousePos.x < rect.size.x):
-			start.y = event.position.y
-			initialPos.y = get_global_position().y
-			initialSize.y = get_size().y
-			resizeY = true
-			isResizing = true
-			print("4 up")
-				
-			
-	if Input.is_action_pressed("LeftMouseDown"):
-		if (isResizing):
-			newWidth = get_size().x
-			newHeight = get_size().y
-			
-			if (resizeX):
-				newWidth = initialSize.x - (start.x - event.position.x)
-			if (resizeY):
-				newHeight = initialSize.y - (start.y - event.position.y)
-				
-			if (initialPos.x != 0):
-				newWidth = initialSize.x + (start.x - event.position.x)
-				set_position(Vector2(initialPos.x - (newWidth - initialSize.x), get_position().y))
-				
-			if (initialPos.y != 0):
-				newHeight = initialSize.y + (start.y - event.position.y)
-				set_position(Vector2(get_position().x, initialPos.y - (newHeight - initialSize.y)))
-			
-			
-			set_size(Vector2(newWidth, newHeight))
-			colShape.shape.size = Vector2(newWidth, newHeight)
-			colShape.global_position = Vector2(get_position().x + (newWidth / 2), get_position().y + (newHeight / 2))
-			print(colShape)
-		
-		
+		if resizeTop:
+			newSize.y += camRelative.y
+			newPos.y -= camRelative.y
+			set_position(newPos)
+
+		set_size(newSize)
+		colShape.shape.size = newSize
+		colShape.global_position = Vector2(get_position().x + (newSize.x / 2), get_position().y + (newSize.y / 2))
+		camPreviousPos = %Player/Camera2D.get_screen_center_position()
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton:
+		mouse_button(event)
+
+	if event is InputEventMouseMotion:
+		mouse_motion(event)
+
+func mouse_button(event: InputEventMouse):
+	var rect: Rect2 = get_global_rect()
+	var localPlayerPos = %Player.position - get_global_position()
+
+	if localPlayerPos.x > 0 && localPlayerPos.y > -100 && localPlayerPos.x < rect.size.x && localPlayerPos.y < 20:
+		deactivated = true
+	elif (localPlayerPos.x < 0 || localPlayerPos.y < -100 || localPlayerPos.x > rect.size.x || localPlayerPos.y > 20) && %Player.is_on_floor():
+		deactivated = false
+
 	if Input.is_action_just_released("LeftMouseDown"):
-		initialPos = Vector2(0, 0)
-		resizeX = false
-		resizeY = false
-		isResizing = false
+		resizeLeft = false
+		resizeRight = false
+		resizeTop = false
+		resizeBottom = false
+		
+	if deactivated:
+		return
+	
+	if Input.is_action_just_pressed("LeftMouseDown"):
+		if is_on_right_border():
+			resizeRight = true
+			print(colShape, "right")
+
+		if is_on_bottom_border():
+			resizeBottom = true
+			print(colShape, "bottom")
+
+		if is_on_left_border():
+			resizeLeft = true
+			print(colShape, "left")
+
+		if is_on_top_border():
+			resizeTop = true
+			print(colShape, "top")
+
+		if resizeRight || resizeLeft || resizeBottom || resizeTop:
+			camPreviousPos = %Player/Camera2D.get_screen_center_position()
+
+func mouse_motion(event: InputEventMouseMotion):
+	if resizeRight || resizeLeft || resizeBottom || resizeTop:
+		var newSize: Vector2 = get_size()
+		var newPos: Vector2 = get_position()
+
+		if resizeRight:
+			newSize.x += event.relative.x
+		if resizeBottom:
+			newSize.y += event.relative.y
+
+		if resizeLeft:
+			newSize.x -= event.relative.x
+			newPos.x += event.relative.x
+			set_position(newPos)
+
+		if resizeTop:
+			newSize.y -= event.relative.y
+			newPos.y += event.relative.y
+			set_position(newPos)
+
+		set_size(newSize)
+		colShape.shape.size = newSize
+		colShape.global_position = Vector2(get_position().x + (newSize.x / 2), get_position().y + (newSize.y / 2))
+		#print(colShape)
+
+	if deactivated:
+		$Mouse.mouse_default_cursor_shape = Control.CURSOR_FORBIDDEN # X
+	elif (is_on_top_border() || is_on_bottom_border()) && !is_on_right_border() && !is_on_left_border():
+		$Mouse.mouse_default_cursor_shape = Control.CURSOR_VSIZE # ↕
+	elif (is_on_left_border() || is_on_right_border()) && !is_on_top_border() && !is_on_bottom_border():
+		$Mouse.mouse_default_cursor_shape = Control.CURSOR_HSIZE # ↔
+	elif is_on_bottom_border() && is_on_left_border() || is_on_top_border() && is_on_right_border():
+		$Mouse.mouse_default_cursor_shape = Control.CURSOR_BDIAGSIZE # ↗
+	elif is_on_top_border() && is_on_left_border() || is_on_bottom_border() && is_on_right_border():
+		$Mouse.mouse_default_cursor_shape = Control.CURSOR_FDIAGSIZE # ↘
+	else:
+		$Mouse.mouse_default_cursor_shape = Control.CURSOR_ARROW
+
+func is_on_left_border() -> bool:
+	var rect: Rect2 = get_global_rect()
+	var pos: Vector2 = get_local_mouse_position()
+
+	return pos.x >= -BORDER_SIZE && pos.x <= BORDER_SIZE && pos.y >= -BORDER_SIZE && pos.y <= rect.size.y + BORDER_SIZE
+
+func is_on_right_border() -> bool:
+	var rect: Rect2 = get_global_rect()
+	var pos: Vector2 = get_local_mouse_position()
+
+	return pos.x >= rect.size.x - BORDER_SIZE && pos.x <= rect.size.x + BORDER_SIZE && pos.y >= -BORDER_SIZE && pos.y <= rect.size.y + BORDER_SIZE
+
+func is_on_top_border() -> bool:
+	var rect: Rect2 = get_global_rect()
+	var pos: Vector2 = get_local_mouse_position()
+
+	return pos.x >= -BORDER_SIZE && pos.x <= rect.size.x + BORDER_SIZE && pos.y >= -BORDER_SIZE && pos.y <= BORDER_SIZE
+
+func is_on_bottom_border() -> bool:
+	var rect: Rect2 = get_global_rect()
+	var pos: Vector2 = get_local_mouse_position()
+
+	return pos.x >= -BORDER_SIZE && pos.x <= rect.size.x + BORDER_SIZE && pos.y >= rect.size.y - BORDER_SIZE && pos.y <= rect.size.y + BORDER_SIZE
