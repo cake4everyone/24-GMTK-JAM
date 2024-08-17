c346776ab989ca74ec8c777373db105d46c74d9f
Niraculix <75939533+Niraculix@users.noreply.github.com>
2024-08-17 18:34:23 +0200

    Wall Jump added

--- player.gd
+++ player.gd
@@ -1,34 +1,71 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1500.0
+const ACCELERATION = 50
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+const WALL_JUMP_PUSHBACK = 500
+const WALL_SLIDE_FRICTION = 100
+
+var sliding = false
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var doubleJump := true
 
 func _physics_process(delta):
-	# Add the gravity.
+	var input_dir: Vector2 = input()
+	
+	if(input_dir != Vector2.ZERO):
+		velocity = velocity.move_toward(SPEED * input_dir, ACCELERATION)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION)
+	
+	velocity.y += gravity
+	if Input.is_action_just_pressed("ui_accept"):
+		jump()
 	if is_on_floor():
 		doubleJump = true
-	else:
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		elif doubleJump:
-			velocity.y = JUMP_VELOCITY * 0.8
-			doubleJump = false
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction: float = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	slide(delta)
 	move_and_slide()
+	
+func input():
+	var input_dir = Vector2.ZERO
+	
+	input_dir.x = Input.get_axis("ui_left","ui_right")
+	input_dir = input_dir.normalized()
+	return input_dir
+	
+func jump():
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif doubleJump && !sliding:
+		velocity.y = JUMP_VELOCITY * 0.8
+		doubleJump = false
+
+	if sliding && Input.is_action_pressed("ui_left"):
+		velocity.y = JUMP_VELOCITY * 0.7
+		velocity.x = WALL_JUMP_PUSHBACK
+		
+	if sliding && Input.is_action_pressed("ui_right"):
+		velocity.y = JUMP_VELOCITY * 0.7
+		velocity.x = -WALL_JUMP_PUSHBACK
+		
+func slide(delta):
+	if is_on_wall() && !is_on_floor():
+		if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left"):
+			sliding = true
+		else:
+			slide_cooldown()
+	else: 
+		slide_cooldown()
+		
+	if sliding:
+		velocity.y += WALL_SLIDE_FRICTION * delta
+		velocity.y = min(velocity.y, WALL_SLIDE_FRICTION)
+		
+func slide_cooldown():
+	await get_tree().create_timer(0.05).timeout
+	sliding = false
+	
