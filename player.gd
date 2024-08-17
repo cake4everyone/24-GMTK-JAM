d6592b550b06a1abbfdb7e8e04d45a194984eb72
Kesuaheli <kesuaheligame@gmail.com>
2024-08-17 17:12:54 +0200

    added double jump
    Co-Authored-By: Niraculix <75939533+Niraculix@users.noreply.github.com>

--- player.gd
+++ player.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var doubleJump := true
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if is_on_floor():
+		doubleJump = true
+	else:
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif doubleJump:
+			velocity.y = JUMP_VELOCITY * 0.8
+			doubleJump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction: float = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
