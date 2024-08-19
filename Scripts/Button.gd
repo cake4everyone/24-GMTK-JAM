f997baa1d1543425425d43500ee5bd38bf5a3547
Kesuaheli <kesuaheligame@gmail.com>
2024-08-19 22:43:20 +0200

    added goal activation
    rerenamed goal

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,15 +1,20 @@
 extends Node2D
 class_name PushButton
 
-var pressed := false
+signal activate
+
+var is_pressed := false
 
 func _on_area_2d_body_entered(body):
 	if body is Player:
-		pressed = true
-		$Sprite2D.frame = 1
+		press()
 
 
 func _on_area_2d_area_entered(area):
 	if area.get_parent() is Platform:
-		pressed = true
-		$Sprite2D.frame = 1
+		press()
+
+func press():
+	is_pressed = true
+	$Sprite2D.frame = 1
+	activate.emit()
--- Scripts/goal.gd
+++ Scripts/goal.gd
@@ -1,6 +1,24 @@
 extends Node2D
 class_name Goal
 
+@export var input: PushButton
+
+var enabled: bool = true
+
+func _ready():
+	if input:
+		enabled = false
+		input.activate.connect(on_input_activate)
+
 func _process(_delta):
+	if enabled:
+		$Flag.frame = 1
+	else:
+		$Flag.frame = 0
+
+func _physics_process(_delta):
 	if %Player in $Area2D.get_overlapping_bodies():
 		pass
+
+func on_input_activate():
+	enabled = true
